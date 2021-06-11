bbd499948c72e9285cf9b32a64369349f524e581
Caroline Tan <carolinectan3@gmail.com>
2021-06-11 10:13:57 -0400

    Refactor to initialize with nums and key of random 5-digits

--- lib/shift.rb
+++ lib/shift.rb
@@ -1,8 +1,14 @@
 class Shift
+  attr_reader :nums, :key
 
-  def initialize(character_set)
-    @character_set = character_set
+  def initialize(nums)
+    @nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+    @key = Array.new
+    until @key.length == 5
+      @key << @nums.sample(1)
+    end
+    @key = @key.join
   end
-
 
 end
