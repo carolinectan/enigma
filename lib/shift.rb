142af8b5214926f909fd788f4ef87b423f8819e0
Caroline Tan <carolinectan3@gmail.com>
2021-06-11 22:33:39 -0400

    Refactor/add final shift method

--- lib/shift.rb
+++ lib/shift.rb
@@ -22,38 +22,15 @@ class Shift
   end
 
   def final_shift
-require "pry"; binding.pry
-    final_shift = {
-      :a => @key[0..1].to_i + @offsets[0].to_i,
-      :b => @key[1..2].to_i + @offsets[1].to_i,
-      :c => @key[2..3].to_i + @offsets[2].to_i,
-      :d => @key[3..4].to_i + @offsets[3].to_i
-    }
-  end
+    self.create_offsets(date = Date.today) # remove later
 
-  # def key_split
-  #   require "pry"; binding.pry
-  #
-  #   key_hash = Hash.new
-  #
-  #   key_hash[:a_key] = @key[0..1]
-  #   key_hash[:b_key] = @key[1..2]
-  #   key_hash[:c_key] = @key[2..3]
-  #   key_hash[:d_key] = @key[3..4]
-  # end
-  #
-  # def offset_hash
-  #   offset_hash = Hash.new
-  #
-  #   offset_hash[:a_offset] = @offsets[0]
-  #   offset_hash[:b_offset] = @offsets[1]
-  #   offset_hash[:c_offset] = @offsets[2]
-  #   offset_hash[:d_offset] = @offsets[3]
-  #
-  #   # a_offset = @offset[0]
-  #   # b_offset = @offset[1]
-  #   # c_offset = @offset[2]
-  #   # d_offset = @offset[3]
-  #   require "pry"; binding.pry
-  # end
+    final_shift = Hash.new
+
+    final_shift[:a] = @key[0..1].to_i + @offsets[0].to_i
+    final_shift[:b] = @key[1..2].to_i + @offsets[1].to_i
+    final_shift[:c] = @key[2..3].to_i + @offsets[2].to_i
+    final_shift[:d] = @key[3..4].to_i + @offsets[3].to_i
+
+    final_shift
+  end
 end
