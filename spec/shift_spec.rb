ed255f55b4719830e103bd0ab24096f3b1a8aaf9
Caroline Tan <carolinectan3@gmail.com>
2021-06-11 21:59:06 -0400

    Draft stubs for keys and offsets

--- spec/shift_spec.rb
+++ spec/shift_spec.rb
@@ -16,6 +16,7 @@ RSpec.describe Shift do
     expect(@shift.nums).to eq([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
     expect(@shift.key).to be_a(String)
     expect(@shift.key.length).to eq(5)
+    expect(@shift.key.to_i).to be_an(Integer)
     expect(@shift.offsets).to eq('')
   end
 
@@ -35,11 +36,38 @@ RSpec.describe Shift do
 
   it 'can create offsets with a date input as a numeric DDMMYY string' do
     expect(@shift.create_offsets('160221')).to eq('8841')
+  end
+
+  it 'can generate keys a b c and d keys as a hash' do
+    # key = double('39927')
+    stubbed_expected = {
+                          :a_key => '39',
+                          :b_key => '99',
+                          :c_key => '92',
+                          :d_key => '27'
+                        }
+    allow(@shift).to receive(:key_split).and_return(stubbed_expected)
+
+    expect(@shift.key_split).to eq(stubbed_expected)
+  end
+
+  it 'can generate a b c and d offsets as a hash' do
+    # key = double('39927')
+    # stubbed_expected = {
+    #                      :a_offset => '39',
+    #                      :b_offset => '99',
+    #                      :c_offset => '92',
+    #                      :d_offset => '27'
+    #                     }
+    # allow(@shift).to receive(:key_split).and_return(stubbed_expected)
+
+    # expect(@shift.key_split).to eq(stubbed_expected)
+    expect(@shift.final_shift).to eq()
+  end
+
 end
   # it 'can test that it returns an offset of 4 numbers' do
   #   allow(@shift).to receive(:key).and_return('05973')
   #
   #   expect(@shift.key).to eq('05973')
   # end
-
-end
