8c1ec071c6d71ff65fcbd455ae367e58e77cf318
Caroline Tan <carolinectan3@gmail.com>
2021-06-13 19:52:28 -0400

    Refactor char_index_pos method to run and store variables

--- modules/arrayable.rb
+++ modules/arrayable.rb
@@ -13,7 +13,10 @@ module Arrayable
     (0..char_array.length - 1).to_a
   end
 
-  def char_index_and_pos(char_array, char_index, pos_in_set)
-    char_array.zip(char_index(char_array), (pos_in_set))
+  def char_index_and_pos(message)
+    char_array = self.char_array(message)
+    pos_in_set = self.positions_in_set(char_array)
+    char_index = self.char_index(char_array)
+    char_index_and_pos = char_array.zip(char_index(char_array), (pos_in_set))
   end
 end
