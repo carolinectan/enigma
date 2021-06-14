d0ea7f3027972cd651593c3c01a677633231be00
Caroline Tan <carolinectan3@gmail.com>
2021-06-14 19:37:48 -0400

    Refactor

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,5 +1,7 @@
 require 'date'
 require_relative 'key'
+require_relative 'encrypt'
+require_relative 'decrypt'
 require_relative '../modules/arrayable'
 require_relative '../modules/hashable'
 
@@ -14,16 +16,7 @@ class Enigma
   end
 
   def encrypt(message, key_arg = 'a', date_arg = Date.today)
-    key = Key.new(key_arg, date_arg)
-    message = self.char_index_and_pos(message.downcase).map do |char, index, pos|
-      if !set.include?(char) then char
-      elsif index % 4 == 0 then @set.rotate(pos + key.final_shift[:a])[0]
-      elsif index % 4 == 1 then @set.rotate(pos + key.final_shift[:b])[0]
-      elsif index % 4 == 2 then @set.rotate(pos + key.final_shift[:c])[0]
-      elsif index % 4 == 3 then @set.rotate(pos + key.final_shift[:d])[0]
-      end
-    end.join
-    message_output('encryption', message, key)
+    Encrypt.new(message, key_arg, date_arg)
   end
 
   def decrypt(message, key_arg = 'a', date_arg = Date.today)
