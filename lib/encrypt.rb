d1be81c836747eeb65d2312f609ea6bd34c07a88
Caroline Tan <carolinectan3@gmail.com>
2021-06-13 23:56:43 -0400

    Removed ARGV[2] and ARGV[3]

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -3,7 +3,7 @@ require_relative '../lib/enigma'
 enigma = Enigma.new
 handle = File.open(ARGV[0], "r")
 message = handle.read
-encrypted = enigma.encrypt(message, ARGV[2], ARGV[3])
+encrypted = enigma.encrypt(message)
 writer = File.open(ARGV[1], "w")
 writer.write(encrypted[:encryption])
 writer.close
